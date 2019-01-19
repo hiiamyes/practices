cd345d5409d9f922520b0bca4f12c6807c72cc61
Yes <joshuayes@gmail.com>
2019-01-20 04:50:54 +0800

    03

---
+++ 03-docker-compose/index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const redis = require("redis");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+// redisClient = redis.createClient();
+app.use(bodyParser.json());
+
+console.log('test2')
+
+// app.post("/signup", (req, res) => {
+//   console.log("signup");
+//   const { username, password } = req.body;
+//   const key = `users:${username}`;
+//   redisClient.hgetall(key, (err, user) => {
+//     if (user === null) {
+//       redisClient.hmset(key, {
+//         username,
+//         password
+//       });
+//       res.json({});
+//     } else {
+//       res.status(409);
+//       res.json({});
+//     }
+//   });
+// });
+
+// app.post("/login", (req, res) => {
+//   console.log("login");
+//   const { username, password } = req.body;
+//   const key = `users:${username}`;
+//   redisClient.hgetall(key, (err, user) => {
+//     if (user === null) {
+//       res.status(401);
+//       res.json({});
+//     } else {
+//       res.json({ message: `Hello ${username}.` });
+//     }
+//   });
+// });
+
+app.listen(port);
