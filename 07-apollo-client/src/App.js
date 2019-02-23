8a66894e62d9bc6502f46820ec14d1a62345f41d
yes <yes@emq.com>
2019-02-23 15:17:41 +0800

    wip

--- 07-apollo-client/src/App.js
+++ 07-apollo-client/src/App.js
@@ -3,11 +3,9 @@ import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import logo from "./logo.svg";
-import "./App.css";
 
 const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io"
+  uri: "http://139.162.47.241:4000"
 });
 
 class App extends Component {
@@ -17,34 +15,18 @@ class App extends Component {
         <Query
           query={gql`
             {
-              rates(currency: "USD") {
-                currency
-                rate
+              books {
+                title
               }
             }
           `}
         >
           {({ loading, error, data }) => {
-            console.log("====================================");
-            console.log("test", loading, error, data, "qq");
-            console.log("====================================");
-            if (loading) return <div>Loading...</div>;
+            console.log(loading, data, error);
             return (
-              <div className="App">
-                <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                  </p>
-                  <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Learn React
-                  </a>
-                </header>
+              <div>
+                <div>{`Loading: ${loading}`}</div>
+                <div>{`data: ${JSON.stringify(data)}`}</div>
               </div>
             );
           }}
