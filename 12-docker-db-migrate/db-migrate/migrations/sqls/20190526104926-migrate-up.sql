02250b2c8d2dd67bbdc39ec913985c1d201efe93
Yes <joshuayes@gmail.com>
2019-05-27 00:43:51 +0800

    practice 12

---
+++ 12-docker-db-migrate/db-migrate/migrations/sqls/20190526104926-migrate-up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    email text,
+    google_open_id text
+);
---
+++ frontend-cookbook/stories/index.stories.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+
+import { Welcome } from "@storybook/react/demo";
+import styled from "styled-components";
+
+const Button = styled.button`
+  min-width: 64px;
+  height: 32px;
+  border-radius: 4px;
+  border: 1px solid black;
+  font-size: 0.875rem;
+  line-height: 2.25rem;
+  text-align: center;
+`;
+
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("Button")} />
+));
+
+storiesOf("Button", module)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>BUTTON</Button>
+  ));
