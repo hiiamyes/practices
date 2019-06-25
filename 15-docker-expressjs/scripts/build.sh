55b0f87bcba65cd8362a51a9a3e6f5c1d0272c1e
Yes <joshuayes@gmail.com>
2019-06-25 21:01:48 +0800

    wip

---
+++ 15-docker-expressjs/scripts/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker build -t practices-10 .
+docker system prune -f
---
+++ 15-docker-expressjs/scripts/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+docker run \
+  -d \
+  --rm \
+  -p 3001:3000 \
+  --name practices-10 \
+  practices-10
