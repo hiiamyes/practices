2a7774f9eb65af0042adb7812818c2259eb0e90f
Yes <joshuayes@gmail.com>
2019-07-08 00:02:46 +0800

    wip

--- frontend-cookbook/components/Input/MoneyInput/index.js
+++ frontend-cookbook/components/Input/MoneyInput/index.js
@@ -1,13 +1,17 @@
 import React, { useState, useLayoutEffect, useEffect } from "react";
-import { throttle } from "lodash/fp";
 import { Decimal } from "decimal.js";
 import numeral from "numeral";
-import FAIcon from "../../FAIcon";
 import Style from "./style";
 import useForceUpdate from "./use-force-update";
 
 const getFormattedValue = ({ value, minUnit }) => {
-  if (value === null || value === undefined || value === "") return "";
+  if (
+    value === null ||
+    value === undefined ||
+    value === "" ||
+    !numeral(value).value()
+  )
+    return "";
   value = String(value);
   if (value === "0") return 0;
   if (minUnit >= 1) {
@@ -26,6 +30,7 @@ const getFormattedValue = ({ value, minUnit }) => {
       value = new Decimal(numeral(value).value()).toFixed(fixed);
     }
     [numberPart, decimalPart = ""] = value.split(decimalPoint);
+    console.log("?:", value);
     return (
       String(numeral(numberPart).format("0,0")) +
       (/\./.test(value) ? decimalPoint : "") +
@@ -47,7 +52,7 @@ const MoneyInput = props => {
 
   useEffect(() => {
     setFormattedValue(getFormattedValue({ value, minUnit }));
-  }, [value]);
+  }, []);
 
   useLayoutEffect(() => {
     const target = inputRef.current;
@@ -68,6 +73,9 @@ const MoneyInput = props => {
           const {
             target: { value, selectionStart }
           } = e;
+          if (e.keyCode === 190 && selectionStart !== value.length) {
+            e.preventDefault();
+          }
           // We should get the chars before and after the caret before the change event
           charBeforeCaret = value.split("")[selectionStart - 1];
           charAfterCaret = value.split("")[selectionStart];
@@ -79,18 +87,41 @@ const MoneyInput = props => {
             target: { value: newValue, selectionStart }
           } = e;
           caretPositionFromEnd = newValue.length - selectionStart;
-          if (keyCode === 8 && charBeforeCaret === ",") {
-            // backspace
-            newValue =
-              newValue.slice(0, selectionStart - 1) +
-              newValue.slice(selectionStart);
-          } else if (keyCode === 46 && charAfterCaret === ",") {
-            // delete
-            caretPositionFromEnd -= 1;
-            newValue =
-              newValue.slice(0, selectionStart) +
-              newValue.slice(selectionStart + 1);
+          switch (keyCode) {
+            case 8:
+              // backspace
+              if (charBeforeCaret === ".") {
+                newValue =
+                  newValue.slice(0, selectionStart - 1) +
+                  "." +
+                  newValue.slice(selectionStart);
+              }
+              if (charBeforeCaret === ",") {
+                newValue =
+                  newValue.slice(0, selectionStart - 1) +
+                  newValue.slice(selectionStart);
+              }
+              break;
+            case 46:
+              // delete
+              if (charAfterCaret === ".") {
+                caretPositionFromEnd -= 1;
+                newValue =
+                  newValue.slice(0, selectionStart) +
+                  "." +
+                  newValue.slice(selectionStart + 1);
+              }
+              if (charAfterCaret === ",") {
+                caretPositionFromEnd -= 1;
+                newValue =
+                  newValue.slice(0, selectionStart) +
+                  newValue.slice(selectionStart + 1);
+              }
+              break;
+            default:
+              break;
           }
+
           if (minUnit > 1 && newValue !== "") {
             if (value === null || value === undefined || value === "") {
               caretPositionFromEnd = minUnit.length - 1;
@@ -114,6 +145,8 @@ const MoneyInput = props => {
           setFormattedValue(newValue);
           if (numeral(newValue).value() !== value) {
             onChange(numeral(newValue).value());
+          } else {
+            forceUpdate();
           }
         }}
       />
@@ -123,15 +156,37 @@ const MoneyInput = props => {
 
 const MoneyInputDemo = () => {
   const [value, setValue] = useState(null);
-  // const [value, setValue] = useState(1234);
+  const [value2, setValue2] = useState(null);
+  const [value3, setValue3] = useState(123);
   return (
     <div>
+      <div>Min unit = 100</div>
       <MoneyInput
         value={value}
         minUnit="100"
         onChange={value => {
           console.log("onChange: ", value);
           setValue(value);
+        }}
+      />
+
+      <div>Min unit = 1</div>
+      <MoneyInput
+        value={value2}
+        minUnit="1"
+        onChange={value => {
+          console.log("onChange: ", value);
+          setValue2(value);
+        }}
+      />
+
+      <div>Min unit = 0.01</div>
+      <MoneyInput
+        value={value3}
+        minUnit="0.01"
+        onChange={value => {
+          console.log("onChange: ", value);
+          setValue3(value);
         }}
       />
     </div>
