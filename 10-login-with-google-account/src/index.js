6602bb4620f82c33f46450c2f96b7648b1bfd8ec
yes <yes@emq.com>
2019-11-08 10:43:26 +0800

    wip

--- 10-login-with-google-account/src/index.js
+++ 10-login-with-google-account/src/index.js
@@ -18,6 +18,24 @@ app.get("/signin", async (req, res) => {
   try {
     const { code } = req.query;
     const user = await getUser(code);
+
+    const {
+      data: { email, id: google_open_id }
+    } = await oauth2.userinfo.v2.me.get({
+      auth: oAuth2Client
+    });
+    const { rows } = await client.query(
+      `
+    insert into 
+      user (email, google_open_id) 
+      values ($1, $2) 
+      on conflict (google_open_id) 
+      do update set email=$1 
+      returning *`,
+      [email, google_open_id]
+    );
+    const user = rows[0];
+
     res.json({ user });
   } catch (error) {
     res.json(`error: ${error}`);
