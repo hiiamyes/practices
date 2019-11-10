72a2751e472c501f4ed56a6e423a617283f6199b
Yes <joshuayes@gmail.com>
2019-11-10 17:18:00 +0800

    wip

--- backend-cookbook/03-google-calendar-api/index.js
+++ backend-cookbook/03-google-calendar-api/index.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 const { google } = require("googleapis");
 const fs = require("fs");
 
@@ -16,13 +16,59 @@ const oAuth2Client = new google.auth.OAuth2(
 oAuth2Client.setCredentials(JSON.parse(token));
 const calendar = google.calendar({ version: "v3", auth: oAuth2Client });
 
-(async () => {
-  //   const d = await calendar.calendarList.list();
-  //   const r = await calendar.calendars.get({ calendarId: "joshuayes@gmail.com" });
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.get("/events", async (req, res) => {
   const { data: events } = await calendar.events.list({
     calendarId: "joshuayes@gmail.com",
     timeMin: new Date("2019-11-01").toISOString(),
     timeMax: new Date("2019-11-30").toISOString()
   });
-  console.log(events);
-})();
+  res.json(events);
+});
+
+app.get("/calendars", async (req, res) => {
+  const { data: calendars } = await calendar.calendarList.list();
+  res.json(calendars);
+});
+
+app.get("/calendar", async (req, res) => {
+  const { data } = await calendar.calendars.get({
+    calendarId: "joshuayes@gmail.com"
+  });
+  res.json(data);
+});
+
+app.get("/watch", (req, res) => {
+  (async () => {
+    try {
+      const { data } = await calendar.events.watch({
+        calendarId: "joshuayes@gmail.com",
+        requestBody: {
+          id: "01234567-89ab-cdef-0123456789ab",
+          type: "web_hook",
+          address: "https://98041173.ngrok.io/n"
+        }
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  })();
+  res.json({});
+});
+
+app.post("/n", (req, res) => {
+  console.log("nnn");
+  res.json({});
+});
+app.listen(process.env.PORT, () =>
+  console.log(`API server starts on port ${process.env.PORT}`)
+);
