33f828db98d25259b71d2d5f5e4ff6962636c94f
yes <yes@emq.com>
2020-01-13 18:30:17 +0800

    wip

--- backend-cookbook/05-k8s-minicute/deploy.sh
+++ backend-cookbook/05-k8s-minicute/deploy.sh
@@ -1,3 +1,4 @@
 eval $(minikube docker-env)
 kubectl apply -f api-deployment.yaml
+kubectl apply -f api-service.yaml
 eval $(minikube docker-env -u)
--- backend-cookbook/05-k8s-minicute/services/api/src/index.js
+++ backend-cookbook/05-k8s-minicute/services/api/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/ping", (req, res) => {
-  res("pong");
+  res.json("pong");
 });
 
 app.listen(3000, () => console.log(`API server starts`));
