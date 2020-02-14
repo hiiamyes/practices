81f1fdf5fa305e734eb04ceb67c5ff30b1ddd7b7
Yes <joshuayes@gmail.com>
2020-02-15 03:07:15 +0800

    wip

--- frontend-cookbook/02-create-chrome-extension/src/content_scripts/index.js
+++ frontend-cookbook/02-create-chrome-extension/src/content_scripts/index.js
@@ -1,45 +1,56 @@
-// const links = document.querySelectorAll(".results a")
-// Array.prototype.forEach.call(links, function(link){
-//   console.log(link)
-// })
-
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { getOr } from "lodash/fp";
 import { render } from "react-dom";
-import osmWaysToGeoJSON from "../osm-ways-to-geojson";
-import Style from "./style";
 
 const App = () => {
   useEffect(() => {
-    console.log("init");
-    if (location.search !== "") {
-      const type = /f=(\w+)&?/.exec(location.search)[1];
-      const dls = document.querySelectorAll(".results dl");
-      Array.prototype.forEach.call(dls, function(dl) {
-        const link = dl.querySelector("dt a");
-        const name = link.text;
-        let [_, date, size] = dl.querySelectorAll("dd span");
-        size = size.textContent;
-        const [amount, unit] = size.split(" ");
-        if (unit === "MB" && +amount > 5000) {
-          if (type === "ssni") {
-            const number = /ssni-(\d\d\d)/i.exec(name)[1];
-            window.open(
-              `https://www.s1s1s1.com/works/detail/ssni${number}`
-            );
-            window.open(link.href);
-          }
-          if (type === "abp") {
-            const number = /abp-(\d\d\d)/i.exec(name)[1];
-            window.open(
-              `https://www.prestige-av.com/goods/goods_list.php?q=abp-${number}&m=search&p=1&s=date`
-            );
-            window.open(link.href);
+    try {
+      console.log("init");
+      if (location.search !== "") {
+        const type = /f=(\w+)&?/.exec(location.search)[1];
+        const dls = document.querySelectorAll(".results dl");
+        Array.prototype.forEach.call(dls, function(dl) {
+          const link = dl.querySelector("dt a");
+          const name = link.text;
+          let [_, date, size] = dl.querySelectorAll("dd span");
+          size = size.textContent;
+          const [amount, unit] = size.split(" ");
+          if (unit === "MB" && +amount > 5000) {
+            if (type === "ssni" && /ssni-(\d\d\d)/i.test(name)) {
+              const [_, number] = /ssni-(\d\d\d)/i.exec(name);
+              if (number) {
+                window.open(
+                  `https://www.s1s1s1.com/works/detail/ssni${number}`
+                );
+                window.open(link.href);
+              }
+            }
+            if (type === "abp" && /abp-(\d\d\d)/i.test(name)) {
+              const [_, number] = /abp-(\d\d\d)/i.exec(name);
+              if (number) {
+                window.open(
+                  `https://www.prestige-av.com/goods/goods_list.php?q=abp-${number}&m=search&p=1&s=date`
+                );
+                window.open(link.href);
+              }
+            }
+            if (type === "stars" && /stars-(\d\d\d)/i.test(name)) {
+              const [_, number] = /stars-(\d\d\d)/i.exec(name);
+              if (number) {
+                window.open(
+                  `https://ec.sod.co.jp/prime/videos/?id=STARS-${number}`
+                );
+                window.open(link.href);
+              }
+            }
           }
           console.log(name, link.href, date.textContent, size);
-        }
-      });
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
   });
   return <div></div>;
