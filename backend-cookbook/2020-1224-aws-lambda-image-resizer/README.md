d9651328beb79a371bb19a21efdb3cdc09c02782
yes <yes@emq.com>
2020-12-25 18:04:24 +0800

    Update image resizer

--- backend-cookbook/2020-1224-aws-lambda-image-resizer/README.md
+++ backend-cookbook/2020-1224-aws-lambda-image-resizer/README.md
@@ -7,16 +7,23 @@
 
 ```
 yarn
-aws --endpoint http://localhost:4569 s3 cp ./image.jpg s3://2020-1224-aws-lambda-image-resizer/image.jpg --profile s3local
 yarn run package
 yarn run invoke:local
 yarn run dev
+aws --endpoint http://localhost:4569 s3 cp ./image.jpg s3://2020-1224-aws-lambda-image-resizer/image.jpg --profile s3local
 ```
 
 ## Deployment
 
 ```
 yarn run deploy
+```
+
+## Production test
+
+```
+aws s3 cp ./image.jpg s3://2020-1224-aws-lambda-image-resizer/image.jpg --profile yeeeeees
+yarn run logs
 ```
 
 ## Reference
@@ -27,3 +34,13 @@ yarn run deploy
 - [google calendar api](https://developers.google.com/calendar/v3/reference)
 
 https://aws.amazon.com/tw/blogs/compute/resize-images-on-the-fly-with-amazon-s3-aws-lambda-and-amazon-api-gateway/
+
+## Error running Sharp inside AWS Lambda function: darwin-x64' binaries cannot be used on the 'linux-x64' platform
+
+'darwin-x64' binaries cannot be used on the 'linux-x64' platform.
+
+https://stackoverflow.com/questions/60181138/error-running-sharp-inside-aws-lambda-function-darwin-x64-binaries-cannot-be-u
+
+```
+npm install --arch=x64 --platform=linux sharp
+```
--- backend-cookbook/2020-1224-aws-lambda-image-resizer/handler.js
+++ backend-cookbook/2020-1224-aws-lambda-image-resizer/handler.js
@@ -9,50 +9,57 @@ const s3 = new aws.S3({
   secretAccessKey: "S3RVER",
   endpoint: new aws.Endpoint("http://localhost:4569"),
 });
-const sharp = require("sharp");
-console.log("setup");
+// const sharp = require("sharp");
+const jimp = require("jimp");
 /**
  *
  * @param {s3 event} event
  * https://docs.aws.amazon.com/lambda/latest/dg/with-s3.html
  */
 module.exports.resize = async (event, context) => {
-  // console.log(event);
-  // console.log(context);
-  // console.log(event.Records[0].s3.object);
-  const bucket = event.Records[0].s3.bucket;
-  const object = event.Records[0].s3.object;
-  // get object
-  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#getObject-property
-  const srcImage = await s3
-    .getObject({
-      Bucket: bucket.name,
-      Key: object.key,
-    })
-    .promise();
-  // console.log("src: ", srcImage);
-  const destImage = await sharp(srcImage.Body)
-    .resize({ width: 320 })
-    .toBuffer();
-  // put object
-  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
-  // console.log("dest: ", destImage);
-  await s3
-    .putObject({
-      Body: destImage,
-      Bucket: 'resize-images',
-      Key: `${object.key.split(".")[0]}-w300.${object.key.split(".")[1]}`,
-    })
-    .promise();
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  try {
+    // console.log(event);
+    // console.log(context);
+    // console.log(event.Records[0].s3.object);
+    const bucket = event.Records[0].s3.bucket;
+    const object = event.Records[0].s3.object;
+    // get object
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#getObject-property
+    const srcImage = await s3
+      .getObject({
+        Bucket: bucket.name,
+        Key: object.key,
+      })
+      .promise();
+    // console.log("src: ", srcImage);
+    let destImage = await jimp.read(srcImage.Body);
+    await destImage.resize(320, jimp.AUTO);
+    // const destImage = await sharp(srcImage.Body)
+    //   .resize({ width: 320 })
+    //   .toBuffer();
+    // put object
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+    destImage = await destImage.getBufferAsync(jimp.MIME_JPEG);
+    console.log("dest: ", destImage);
+    await s3
+      .putObject({
+        Body: destImage,
+        Bucket: "2020-1224-aws-lambda-image-resizer-resize-images",
+        Key: `${object.key.split(".")[0]}-w300.${object.key.split(".")[1]}`,
+      })
+      .promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Go",
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
