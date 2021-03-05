fbf41f94bde6e842f925810e6dada0814857ca68
Yes Lee <joshuayes@gmail.com>
2021-03-06 00:38:06 +0800

    wip

--- backend-cookbook/2021-0301-url-parser/src/index.js
+++ backend-cookbook/2021-0301-url-parser/src/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const open = require('open');
 const puppeteer = require("puppeteer");
+const { compact } = require('lodash/fp')
+const regexp = require('./regexp');
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -9,11 +11,16 @@ const puppeteer = require("puppeteer");
     await page.goto(process.env.SOURCE_URL);
     const numbers = (await page.$$eval('.torrent-list tbody tr td:nth-child(2) a', as => as.map(a => a.text))).map(
       aText => {
-        // console.log(aText);
-        return new RegExp(process.env.REGEXP).exec(aText)[1]
+        try {
+          // console.log(aText);
+          // console.log(new RegExp(regexp).exec(aText));
+          return new RegExp(regexp).exec(aText)[1]
+        } catch (error) {
+          return null
+        }
       }
     )
-    for (const number of numbers) {
+    for (const number of compact(numbers)) {
       const url = `${process.env.DESTINATION_URL}/${number}/`;
       // console.log(url)
       await open(url)
