4f30c88fd441e93863ca157a3a021289ca6ddf66
Yes Lee <joshuayes@gmail.com>
2021-03-14 01:38:08 +0800

    wip

--- backend-cookbook/2021-0301-url-parser/src/index.js
+++ backend-cookbook/2021-0301-url-parser/src/index.js
@@ -1,32 +1,33 @@
 require("dotenv").config();
-const open = require('open');
+const open = require("open");
 const puppeteer = require("puppeteer");
-const { compact } = require('lodash/fp')
-const regexp = require('./regexp');
+const { compact } = require("lodash/fp");
+const regexp = require("./regexp");
 
-(async () => {
+const COUNT = 30(async () => {
   const browser = await puppeteer.launch();
   try {
     const page = await browser.newPage();
     await page.goto(process.env.SOURCE_URL);
-    const numbers = (await page.$$eval('.torrent-list tbody tr td:nth-child(2) a', as => as.map(a => a.text))).map(
-      aText => {
-        try {
-          // console.log(aText);
-          // console.log(new RegExp(regexp).exec(aText));
-          return new RegExp(regexp).exec(aText)[1]
-        } catch (error) {
-          return null
-        }
+    const numbers = (
+      await page.$$eval(".torrent-list tbody tr td:nth-child(2) a", (as) =>
+        as.map((a) => a.text),
+      )
+    ).map((aText) => {
+      try {
+        // console.log(aText);
+        // console.log(new RegExp(regexp).exec(aText));
+        return new RegExp(regexp).exec(aText)[1];
+      } catch (error) {
+        return null;
       }
-    )
-    for (let i = 0; i < 15; i++) {
-      const number = compact(numbers)[i]
+    });
+    for (let i = 0; i < COUNT; i++) {
+      const number = compact(numbers)[i];
       const url = `${process.env.DESTINATION_URL}/${number}/`;
       // console.log(url)
-      await open(url)
+      await open(url);
     }
-
   } catch (error) {
     console.log(error);
   } finally {
