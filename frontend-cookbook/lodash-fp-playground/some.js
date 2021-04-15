23abe1cf04957959dc71e1b1a104c57fb74c59be
Yes Lee <joshuayes@gmail.com>
2021-04-15 16:47:41 +0800

    wip

--- frontend-cookbook/lodash-fp-playground/some.js
+++ frontend-cookbook/lodash-fp-playground/some.js
@@ -1,8 +1,13 @@
 const { some, includes } = require("lodash/fp");
 
 const paths = ["/nodes", "/paths", "/m/nodes"];
-const base = "/nodes";
 
-console.log(some(includes('/nodes'), paths))
-console.log(some(includes('/nodesq'), paths))
-console.log(some(includes('/m/nodes'), paths))
+console.log(some(includes("/nodes/123"), paths));
+console.log(
+  some((qq) => {
+    console.log(qq);
+    return includes(qq, "/nodes/123");
+  }, paths),
+);
+console.log(some(includes("/nodesq"), paths));
+console.log(some(includes("/m/nodes"), paths));
