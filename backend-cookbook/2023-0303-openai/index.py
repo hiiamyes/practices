b5a0d30f1242b9d190609ed7c619c2352e7039a8
Yes Lee <joshuayes@gmail.com>
2023-03-08 15:05:59 +0800

    wip

--- backend-cookbook/2023-0303-openai/index.py
+++ backend-cookbook/2023-0303-openai/index.py
@@ -1,8 +1,14 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # import openai
-# # openai.api_key = ""
+# # openai.api_key = "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"
 
 
-# openai.api_key = ""
+# openai.api_key = "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"
 # audio_file = open("./i.m4a", "rb")
 # transcript = openai.Audio.transcribe("whisper-1", audio_file, "zh")
 # # transcript = openai.Audio.transcribe({
@@ -15,10 +21,9 @@
 # print('q')
 # print(transcript)
 
+print(os.getenv('KEY'))
 
-import os
-import openai
-openai.api_key = ""
+openai.api_key = os.getenv('KEY')
 
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
